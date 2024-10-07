072694de4a80f009aceb591c0ad3fda11b0f3e2d
DAM <aterik.dev@gmail.com>
2024-10-07 09:27:17 +0200

    Primer Commit

---
+++ src/main/java/com/erikat/practica_json/InitApp.java
@@ -0,0 +1,23 @@
+package com.erikat.practica_json;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InitApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(InitApp.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
